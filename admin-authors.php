c74c84185b04041db18fbdad115d703e7a276c4f
root <root@medievalsourcesbibliography.org>
2022-12-17 11:52:00 -0500

    working on authors

--- admin-authors.php
+++ admin-authors.php
@@ -1,250 +1,43 @@
 <?php
-include('header.php');
-include('connect.php'); 
-require_once('auth.php');
+require_once 'auth.php';
+include 'header.php';
+require_once 'class-authors.php';
 
-if(isAppLoggedIn()) { 
+$authors = new OMSB\Authors;
 
-    if (!$_POST && !$_GET) {      ####  new author form  ####
-        if($_SESSION['username'] == 'morgan' || $_SESSION['username'] == 'elc') {
-    	   display_form(0, "Add a New Author", "Create New Author");
-        } else {
-            echo "Sorry, you do not have sufficient privileges to create new authors.";
-        }
+if ( isset( $_SESSION['user'] ) && ! empty( $_SESSION['user'] && ! isset( $_SESSION['user']->error ) ) ) {
+
+  if ( empty( $_POST ) && empty( $_GET ) ) {
+    $user = ( $_SESSION['user'] );
+    if ( in_array( 'administrator', $user->user_roles ) ) {
+      // Display an empty form to create a new author.
+      echo $authors->author_form( false );
+    } else {
+      echo '<p class="error">You do not have sufficient permissions to create new authors</p>';
     }
 
-    if ( !empty($_POST) ) {             ####  we already have an ID so we are editing  ####
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";        
-        // must protect against 31336 hax0rz, zomg! (the 6 is on purpose--this won't stop sophisticated hacks)
-        foreach ($_POST as $key => $post_field) {
-            if(strlen($post_field) > 200 && ( $key != 'bio') )
-
-                die("You submitted a post search term that was too long--please alert the web master and include the URL from your browser's location bar.");
-        }
-        if ( $_POST['id'] ) {             ####  we already have an ID so we are editing  ####
-    		$id=$_POST['id'];
-            $labels = array( 
-                "id" => "id",
-                "name" => "Name",
-                "alias" => "Alias",
-                "title" => "Title",
-                "date_type" => "date_type",
-                "date_circa" => "date_circa",
-                "date_begin" => "date_begin",
-                "date_end" => "date_end",
-                "bio" => "bio"
-                );
-
-            foreach($labels as $field => $value)
-            {
-                $good_data[ $field] = 
-                strip_tags( trim( $_POST[$field] ) ) ;    
-                $good_data[$field] = 
-                        mysqli_real_escape_string($db_server, $good_data[$field]);
-            }
-            $query = "UPDATE authors set
-                name       = '$good_data[name]',
-                alias      = '$good_data[alias]',
-                title      = '$good_data[title]', 
-                updated_at = '".date("Y-m-d H:i:s")."',
-                date_type  = '$good_data[date_type]',
-                date_circa = '$good_data[date_circa]',
-                date_begin = '$good_data[date_begin]',
-                date_end   = '$good_data[date_end]',
-                bio        = '$good_data[bio]'
-                where id=$id;";
-            $result = mysqli_query($db_server,$query)
-                or die ("Couldn't execute query:"
-                    .mysqli_error($db_server));
-            if(mysqli_affected_rows($db_server) > 0)
-            {
-				display_form($_POST, "{$_POST['name']} has been updated.", "Submit Changes");
-            }
-            else
-				display_form($_POST, "No changes made.", "Submit Changes");
-
-    	} else {             ####  we don't have an ID yet because we are inserting a new author & display that info  ####
-    		$labels = array( 
-                    "name" => "Name",
-                    "alias" => "Alias",
-                    "title" => "Title",
-                    "date_type" => "date_type",
-                    "date_circa" => "date_circa",
-                    "date_begin" => "date_begin",
-                    "date_end" => "date_end",
-                    "bio" => "bio",
-                    );
-
-            foreach($labels as $field => $value)
-            {
-            $good_data[ $field] = 
-                strip_tags( trim( $_POST[$field] ) ) ;                  
-                $good_data[$field] = mysqli_real_escape_string($db_server, $good_data[$field]);
-            }
-            $query = "INSERT INTO authors (
-                name, 
-                alias, 
-                title, 
-                date_type,
-                date_circa,
-                date_begin,
-                date_end,
-                bio
-            )
-            VALUES (
-                '$good_data[name]',
-                '$good_data[alias]',
-                '$good_data[title]', 
-                '$good_data[date_type]',
-                '$good_data[date_circa]',
-                '$good_data[date_begin]',
-                '$good_data[date_end]',
-                '$good_data[bio]'
-            );";
-
-            $result = mysqli_query($db_server,$query)
-                or die ("Couldn't execute query:"
-                    .mysqli_error($db_server));
-            if(mysqli_affected_rows($db_server) > 0) {
-                $_POST['id'] = mysqli_insert_id($db_server);
-    			display_form($_POST, "{$_POST['name']} has been added.", "Submit Changes");
-    			echo "<a href=\"authors.php?id={$_POST['id']}\">View Author Record</a>.";
-            }
-            else
-    			display_form($_POST, "No author added", "Submit Changes");
 	}
-    } elseif ( !empty($_GET) ) {
-        // must protect against 31336 hax0rz, zomg! (the 6 is on purpose--this won't stop sophisticated hacks)
-        foreach ($_GET as $get_field) {
-            if(strlen($get_field) > 120 )
-                die("You submitted a search term that was too long--please alert the web master and include the URL from your browser's location bar.");
-        }
-#        if ( $_GET ) {         ####  we have $_GET data from URL -- edit or delete author ####
-        	if ( $_GET['delete'] ) {    ## we are going to delete this author ##
-        		echo "<h2>Delete Author</h2>";
 
-        		$id=mysqli_real_escape_string($db_server, $_GET['delete']);
-        		$result = mysqli_query($db_server, "select * from authors where id=$id;");
+  if ( ! empty( $_POST ) ) {
+    // We are submitting a form, so update or create an author
+    if ( isset( $_POST['id'] ) && '' !== $_POST['id'] ) {
+      $authors->update();
+    } else {
+      $authors->create();
+    }
+  }
+  //elseif ( ! empty( $_GET ) ) {
+  //   // Delete a source or show the form to edit a source
+  //   if ( isset( $_GET['delete'] ) && '' !== $_GET['delete'] ) {
+  //     $source->delete();
+  //   } elseif( isset( $_GET['id'] ) ) {
+  //     echo $source->source_form( true );
+  //   }
+  // }
 
-        		if ( !$result->num_rows ) {
-          			print ("Could not find that author. <br>");
-          			print ("<a href=\"authors.php\">Go to Search Page</a>.");
-        		} else {
 
-        			$author = mysqli_fetch_array($result);
-        			$name = $author['name'];
-        			?>
-
-        			<?php
-        			$result = mysqli_query($db_server, "delete from authors where id=$id;");
-        			if(mysqli_affected_rows($db_server) > 0) {
-        			?>
-        				<p><?php echo $name; ?> has been removed from the database.</p>
-        				<?php
-        			} else {
-        				print ("Sorry, we couldn't delete that author.");
-        			}  # end successful delete
-        		}  # end valid search result
-
-        	} elseif( $_GET['id'] && !$_POST) {        ####  we have _GET['id'] & no _POST data -- just display edit author form with ID's info #####
-        		$id=mysqli_real_escape_string($db_server, $_GET['id']);
-        		$result = mysqli_query($db_server, "select * from authors where id=$id;");
-
-        		if ( !$result->num_rows ) {
-          			print ("Could not find that author. <br>"); 
-          			print ("<a href=\"authors.php\">Go to Search Page</a>.");
-        		} else {
-        			$author = mysqli_fetch_array($result);
-        			$name = $author['name'];
-        			$alias = $author['alias'];
-        			$title = $author['title'];
-        			$date_type = $author['date_type'];
-        			$date_circa = $author['date_circa'];
-        			$date_begin = $author['date_begin'];
-        			$date_end = $author['date_end'];
-        			$bio = $author['bio'];
-
-        			display_form($author, "Edit Author Details", "Submit Changes");
-        		}  # end valid search result
-            } else {    #### we don't have a valid GET request!  ####
-                echo "You sent a request the server could not understand; please try again.";
-                echo "<!--No, I don't understand that banter at all!-->";
-        	} # end if -- edit or delete
-        }  # end _GET data
-#    }
-} else { ?>
-    <p>Sorry, you must <a href="/login.php">log in</a> to view this page.</p>
-<?php }
-
-####  function to display the form  ####
-function display_form($data, $legend, $button){
-?>
-		<form id="admin-author"  action='admin-authors.php<?php if ( $data['id'] ) echo "?id=$data[id]";?>' method='POST'>
-        <fieldset>
-                <legend><?php echo $legend?></legend>
-                <ul>
-                                <input id="id" name="id" type="hidden" value=<?php echo $data['id'];?>></li>
-                        <li class="whole"><label for="name">Name</label>
-                                <input id="name" name="name" type="text" placeholder="Author's name as found in DMA or WorldCat" value="<?php echo $data['name'];?>" autofocus></li>
-                        <li class="whole"><label for="alias">Alias</label>
-                                <input id="alias" name="alias" type="text" placeholder="Alternate names or spellings" value="<?php echo $data['alias'];?>"></li>
-                        <li class="whole"><label for="title">Title</label>
-                                <input id="title" name="title" type="text" placeholder="Title, such as Saint, King, Bishop, etc." value="<?php echo $data['title'];?>" ></li>
-                        <li class="checkbox fourth"><label for="date_circa">Date circa</label>
-                                <input id="date_circa" name="date_circa" type="checkbox" value="1" <?php if ( $data['date_circa'] ) echo "checked";?>></li>
-                        <li class="fourth"><label for="date_type">Date modifier</label>
-                                <input id="date_type" name="date_type" type="text" placeholder="circa, died, flourished, reigned" value="<?php echo $data['date_type'];?>"></li>
-                        <li class="fourth"><label for="date_begin">From</label>
-                                <input id="date_begin" name="date_begin" type="text" placeholder="begin date" value="<?php echo $data['date_begin'];?>"></li>
-                        <li class="fourth"><label for="date_end">To</label>
-                                <input id="date_end" name="date_end" type="text" placeholder="end date"value="<?php echo $data['date_end'];?>"></li>
-                        <li class="whole"><label for="bio">Biographical information</label>
-                                <p><a href="#" onclick="toggle_visibility('format');">Text formatting instructions >></a></p>
-                                        <script type="text/javascript">
-                                        <!--
-                                            function toggle_visibility(id) {
-                                               var e = document.getElementById(id);
-                                               if(e.style.display == 'block')
-                                                  e.style.display = 'none';
-                                               else
-                                                  e.style.display = 'block';
-                                            }
-                                        //-->
-                                        </script>
-                                <div id="format">
-                                    <h4>Basic Formatting:</h4>
-                                    <table>
-                                      <tr>
-                                        <th>You type:</td>
-                                        <th>Looks like:</td>
-                                      </tr><tr>
-                                        <td>_italics_</td>
-                                        <td><i>italics</i></td>
-                                      </tr><tr>
-                                        <td>*bold*</td>
-                                        <td><b>bold</b></td>
-                                      </tr><tr>
-                                        <td>"Google":http://google.com</td>
-                                        <td><a href="http://google.com">Google</a></td>
-                                      </tr><tr>
-                                        <td>* an item <br /> * another item <br />  * and another </td>
-                                        <td><ul><li>an item</li><li>another item</li><li>and another</li></ul></td>
-                                      </tr><tr>
-                                        <td># item one <br /> # item two <br /> # item three </td>
-                                        <td><ol><li>item one</li><li>item two</li><li>item three</li></ol></td>
-                                      </tr>
-                                    </table>
-                                </div>
-                                <textarea id="bio" name="bio" rows="5" placeholder="Biographical information"><?php echo $data['bio'];?></textarea></li>
-                </ul>
-        </fieldset>
-        <input type='submit' class="button" value='<?php echo $button;?>' />
-	</form>
-
-<?php
+} else {
+  echo '<p class="error">You must be logged in to view this page.</p>';
 }
 
-include ('footer.php'); ?>
-
+include 'footer.php';
--- class-authors.php
+++ class-authors.php
@@ -3,9 +3,13 @@ namespace OMSB;
 
 require_once 'class-list.php';
 require_once 'class-database.php';
+require_once 'class-results.php';
+require_once 'vendor/textile/src/Netcarver/Textile/Parser.php';
 
 use OMSB\ListClass;
 use OMSB\Database;
+use OMSB\Results;
+use Netcarver\Textile\Parser;
 
 class Authors extends ListClass {
 
@@ -15,7 +19,9 @@ class Authors extends ListClass {
 
   public function __construct() {
     $this->table_name = 'authors';
-    $this->db         = new Database;
+    $this->db         = new Database();
+    $this->results    = new Search_Results();
+    $this->textile    = new Parser();
   }
 
   /*
@@ -77,6 +83,11 @@ class Authors extends ListClass {
     return $details;
   }
 
+  /*
+  * Get an author from the ID.
+  *
+  * @param int $id ID of the author.
+  */
   public function get_author_by_id( $id ) {
     $author_query = sprintf( "SELECT name from authors WHERE id=%d", intval( $id ) );
     $result       = $this->db->mysqli->query( $author_query );
@@ -92,5 +103,197 @@ class Authors extends ListClass {
     return $input;
   }
 
+  /*
+  * Show the form to create or edit an author.
+  *
+  * @param bool $edit Whether we are editing an existing author.
+  */
+  public function author_form( $edit = true ) {
+    $data = $this->fields_array();
+
+    $title    = $edit ? 'Update AUTHORNAME' : 'Add a new author';
+    $id_param = '';  // @todo - if this isn't a new author, put "id=$id" here
+    $date_checked = $data['date_circa'] ? "checked='checked'" : '';
+    $submit_text = $edit ? 'Update Author' : 'Create Author';
+
+    echo "<h2>{$title}</h2>
+      <form id='admin-author' action='admin-authors.php{$id_param}' method='POST'>
+        <ul>
+          <li>
+            <input id='id' name='id' type='hidden' value='{$data['id']}'>
+          </li>
+          <li class='whole'>
+            <label for='name'>Name</label>
+            <input id='name' name='name' type='text' aria-describedby='author-description' value='{$data['name']}' autofocus>
+            <p class='description author-description'>Author's name as found in DMA or WorldCat</p>
+          </li>
+          <li class='whole'>
+            <label for='alias'>Alias</label>
+            <input id='alias' name='alias' type='text' aria-describedby='alias-description' value='{$data['alias']}'>
+            <p class='description alias-description'>Alternate names or spellings</p>
+          </li>
+          <li class='whole'>
+            <label for='title'>Title</label>
+            <input id='title' name='title' type='text' aria-describedby='title-description' value='{$data['title']}'>
+            <p class='description title-description'>Title, such as Saint, King, Bishop, etc.</p>
+          </li>
+          <li class='checkbox fourth'>
+            <label for='date_circa'>Date circa</label>
+            <input id='date_circa' name='date_circa' type='checkbox' value='1' {$date_checked}></li>
+          <li class='fourth'><label for='date_type'>Date modifier</label>
+            <select id='date_type' name='date_type' value='{$data['date_type']}'>
+              <option value='circa'>Circa</option>
+              <option value='died'>Died</option>
+              <option value='flourished'>Flourished</option>
+              <option value='reigned'>Reigned</option>
+            </select>
+          </li>
+          <li class='fourth'>
+            <label for='date_begin'>From</label>
+            <input id='date_begin' name='date_begin' type='text' value='{$data['date_begin']}'>
+          </li>
+          <li class='fourth'>
+            <label for='date_end'>To</label>
+            <input id='date_end' name='date_end' type='text' value='{$data['date_end']}'>
+          </li>
+          <li class='whole'>
+            <label for='bio'>Biographical information</label>
+            <textarea id='bio' name='bio' rows='5'>{$data['bio']}</textarea>
+          </li>
+        </ul>
+        <input type='submit' class='button' value='{$submit_text}' />
+    	</form>";
+  }
+
+  public function update() {
+
+  }
+
+  public function create() {
+    $data = $this->sanitize_input();
+
+    $query = "INSERT INTO authors (
+        name,
+        alias,
+        title,
+        date_type,
+        date_circa,
+        date_begin,
+        date_end,
+        bio
+    )
+    VALUES (
+        '$data[name]',
+        '$data[alias]',
+        '$data[title]',
+        '$data[date_type]',
+        '$data[date_circa]',
+        '$data[date_begin]',
+        '$data[date_end]',
+        '$data[bio]'
+    );";
+
+    $result = $this->db->mysqli->query( $query );
+
+    if ( ! $result ) {
+      echo '<p class="error">Database error.  Please report the following error to the administrator: <pre>' . print_r( $this->db->mysqli->error, true ) . '</pre></p>';
+    } else {
+      if ( isset( $_POST['id'] ) && '' !== $_POST['id'] ) {
+        $id = $_POST['id'];
+      } else {
+        $id = $this->db->mysqli->insert_id;
+      }
+
+      echo "<p class='success'>{$_POST['name']} has been added.<br />
+      <a href='/authors.php?id={$id}'>View Author</a>.<br .>
+      <a href='/admin-authors.php?id={$id}'>Edit Author</a>.</p>";
+    }
+  }
+
+  public function display_author( $id ) {
+    $id     = mysqli_real_escape_string($db_server, $_GET['id']);
+    $query  = "select * from authors where id=$id;";
+    $result = $this->db->mysqli->query( $query );
+
+    if ( ! $result ) {
+      echo '<p class="error">No author found with id ' . $id . '</p>';
+    } else {
+      $author = mysqli_fetch_array( $result );
+
+      $alias      = $author['alias'] ?? '';
+      $title      = $author['title'] ?? '';
+      $date_type  = $author['date_type'] ?? '';
+      $date_circa = $author['date_circa'] ? 'c.&nbsp;' : '';
+      $bio        = $author['bio'] ?? '';
+      $works      = $this->get_works_by_author( $id );
+
+      if ( isset( $author['date_begin'] ) && '' !== $author['date_begin'] && isset( $author['date_end'] ) && '' !== $author['date_end'] ) {
+        $date = $author['date_begin'] . ' - ' . $author['date_end'];
+      } elseif ( isset( $author['date_begin'] ) && '' !== $author['date_begin'] ) {
+        $date = $author['date_begin'];
+      } else {
+        $date = $author['date_end'];
+      }
+
+      echo "
+      <h2>Author Details</h2>
+      <article class='author'>
+      <h4><span class='name'>{$author['name']}</span> {$alias} {$title}</h4>
+      <p>{$date_type} {$date}</p>
+
+      <div class='bio'>
+          {$this->textile->parse( $bio )}
+      </div>
+
+      <div class='works'>
+            <h5>OMSB Records by {$name}</h5>
+            {$works}
+        </div>
+      </article>";
+    }
+  }
+
+  public function get_works_by_author( $id ) {
+    $id     = mysqli_real_escape_string($db_server, $_GET['id']);
+    $query  = "select source_id from authorships where author_id=$id;";
+    $result = $this->db->mysqli->query( $query );
+    $rows = $result->fetch_all( MYSQLI_ASSOC );
+    return $this->results->display_results( $rows );
+  }
+
+  public function fields_array() {
+    return array(
+      'id' => '',
+      'name' => '',
+      'alias' => '',
+      'title' => '',
+      'date_circa' => '',
+      'date_type' => '',
+      'date_begin' => '',
+      'date_end' => '',
+      'bio' => '',
+    );
+  }
+
+  public function sanitize_input() {
+    $fields = $this->fields_array();
+    foreach( $fields as $field => $value ) {
+      if ( isset( $_POST[ $field ] ) && '' !== $_POST[ $field ] ) {
+        $fields[ $field ] = $this->db->mysqli->real_escape_string( strip_tags( trim( $_POST[ $field ] ) ) );
+      }
+    }
+
+    return $fields;
+  }
+
+  public function author_search_form() {
+    ?>
+  	<h2>Search for Medieval Authors</h2>
+      <form action="authors.php" method="get">
+      <li class="half"><label>Search:</label> <input type="text" name="search" placeholder="type an author's name"/></li>
+      <input type="submit" class="button" value="Search" />
+      </form>
+  	<?php
+  }
+
 }
-?>

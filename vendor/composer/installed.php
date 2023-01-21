7d548ff93df1eccd8a758688def86e4fdd1ff198
Morgan Kay <morgan@wpalchemists.com>
2023-01-21 09:00:58 -0800

    install php textile with composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9fd2ad9006313ec3d43de279233d3287db32607d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9fd2ad9006313ec3d43de279233d3287db32607d',
+            'dev_requirement' => false,
+        ),
+        'netcarver/textile' => array(
+            'pretty_version' => 'v4.0.0',
+            'version' => '4.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../netcarver/textile',
+            'aliases' => array(),
+            'reference' => '79b7d5102816ff017a940d8695a35d7f8db1e60a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
